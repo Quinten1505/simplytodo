9ab829ed103ce81cee8f747d2394e0dba459dc5b
Quinten <Quinten1505@gmail.com>
2023-03-04 18:43:04 +0100

    exported the TodoItem class

---
+++ static/js/TodoItem.js
@@ -0,0 +1,34 @@
+/**
+ * @typedef {Object} TodoItemOptions
+ * @property {string} textValue - The text value of the todo item
+ */
+
+class TodoItem {
+    constructor(options) {
+        this.options = options
+        this.elements = {
+            main: document.createElement("div"),
+            iconContainer: document.createElement("div"),
+            icon: document.createElement("div"),
+            text: document.createElement("div"),
+        }
+
+        this.elements.main.classList.add("todo-item")
+
+        this.elements.iconContainer.classList.add("todo-item__icon-container")
+        this.elements.main.appendChild(this.elements.iconContainer)
+        this.elements.icon.classList.add("todo-item__icon-container__icon")
+        this.elements.iconContainer.appendChild(this.elements.icon)
+
+        this.elements.text.classList.add("todo-item__text")
+        this.elements.text.innerText = this.options.textValue
+        this.elements.main.appendChild(this.elements.text)
+    }
+
+    createItem() {
+        const todoList = document.querySelector(".todo-list")
+        todoList.appendChild(this.elements.main)
+    }
+}
+
+export default TodoItem
--- static/js/todo.js
+++ static/js/todo.js
@@ -1,17 +1,15 @@
+import TodoItem from './TodoItem.js';
+
 const inputField = document.querySelector('.add-item__input');
-const todoList = document.querySelector('.todo-list');
 
 // Add event listener to input field for the enter key press
 inputField.addEventListener('keypress', function (e) {
     if (e.keyCode === 13) {
-        // Create a new li element
-        const li = document.createElement('li');
-        // Add the class name to the li element
-        li.className = 'todo-list__item';
-        // Add the text from the input field to the li element
-        li.appendChild(document.createTextNode(inputField.value));
-        // Append the li element to the ul element
-        todoList.appendChild(li);
+        // Create a new todo item
+        const todoItem = new TodoItem({
+            textValue: inputField.value
+        });
+        todoItem.createItem();
         // Clear the input field
         inputField.value = '';
     }
